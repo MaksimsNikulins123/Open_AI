c60d4f3713650d0eadea53fc70bdb98c3078026d
Max <maksim.nikulin.developer@gmail.com>
2023-11-14 22:53:29 +0200

    Map articles

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -1,46 +1,64 @@
 import { useState, useEffect } from "react";
 
-import { copy, linkIcon, loader, tick } from '../assets';
+import { copy, linkIcon, loader, remove, tick } from '../assets';
 
 import { useLazyGetSummaryQuery } from "../services/article";
 
 const Demo = () => {
-  
+
   const [article, setArticle] = useState({
     url: '',
+    length: 3,
     summary: '',
   });
 
+  const [allArticles, setAllAricles] = useState([]);
+
   const [getSummary, { error, isFetching }] = useLazyGetSummaryQuery();
 
+  useEffect(() => {
+    const articlesFromLocalStorage = JSON.parse(localStorage.getItem('articles'))
+
+    if (articlesFromLocalStorage) {
+      setAllAricles(articlesFromLocalStorage)
+    }
+  }, []);
 
   const handleSubmit = async (e) => {
 
-      e.preventDefault();
- 
-    const { data } = await getSummary({ 
+    e.preventDefault();
+
+    const { data } = await getSummary({
       articleUrl: article.url,
+      articleLength: article.length,
 
     });
 
-    if(data?.summary) {
+    if (data?.summary) {
       const newArticle = {
         ...article,
         summary: data.summary
       };
 
+      const updateAllArticles = [
+        newArticle,
+        ...allArticles
+      ];
+
       setArticle(newArticle);
-      
-      console.log(newArticle);
+      setAllAricles(updateAllArticles)
+
+      localStorage.setItem('articles', JSON.stringify(updateAllArticles))
+
     }
   }
 
-  
+
 
 
 
   return (
-    <section className="mt-16 w-full max-w-xl">
+    <section className="mt-12 w-full max-w-xl">
       {/* Search */}
       <div className="flex flex-col w-full gap-2">
         <form
@@ -74,6 +92,37 @@ const Demo = () => {
         </form>
 
         {/* Browse URL History */}
+        <div className="flex flex-col gap-1 max-h-60 overflow-y-auto">
+          {allArticles.map((item, index) => (
+        <div className="flex justify-between items-center">
+              <div
+              key={`link-${index}`}
+              onClick={() => setArticle(item)}
+              className="link_card"
+            >
+              <div className="copy_btn">
+                <img 
+                  src={copy}
+                  alt="copy_icon"
+                  className="w-[40%] h-[40%] object-contain"
+                />
+              </div>
+              <p>
+                {item.url}
+              </p>
+              
+            </div>
+            <div className="remove_btn">
+                <img 
+                  src={remove}
+                  alt="remove_icon"
+                  className="w-[80%] h-[80%] object-contain"
+                  onClick={() => {alert('Delete')}}
+                />
+              </div>
+        </div>
+          ))}
+        </div>
       </div>
 
       {/* Display Results */}
